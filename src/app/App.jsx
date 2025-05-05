9de821da74ac66f370476a157ac36a59d24e05c5
Harsha-lakmal <lakmalharsha424@gmail.com>
2025-05-05 21:50:51 +0530

    develop to petzonlanka

---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
+
+function App() {
+  return (
+   <div>
+    <h1>Name is Harsha Lakmal</h1>
+   </div>
+
+  );
+}
+
+export default App;
